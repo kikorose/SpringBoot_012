045097411b2963657b4921393a4b7f4e1a5ae9bb
kikorose <nakelia78@gmil.com>
2017-08-23 10:52:57 -0400

    first Commit(uploading image with cloudinary)

---
+++ src/main/java/com/example/demo/DemoSpringBoot012Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoot012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBoot012Application.class, args);
+	}
+}
